eaa2397e005b75693c0443259dacd4804f7427ea
BL00DY-C0D3 <151839541+BL00DY-C0D3@users.noreply.github.com>
2023-12-22 15:32:22 +0100

    Update start-echo.sh

--- scripts/start-echo.sh
+++ scripts/start-echo.sh
@@ -17,5 +17,7 @@ mkdir /ready-at-dawn-echo-arena/logs/$HOSTNAME/old 2> /dev/null
 #move old log files
 mv /ready-at-dawn-echo-arena/logs/$HOSTNAME/*.log /ready-at-dawn-echo-arena/logs/$HOSTNAME/old
 
+#export the DISPLAY to a virtual display
+export DISPLAY=:0.0 # Select screen 0.0
 #start the echo server process
 nohup /usr/bin/wine /ready-at-dawn-echo-arena/bin/win10/echovr.exe $flags &
