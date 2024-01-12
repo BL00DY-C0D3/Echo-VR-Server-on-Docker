f338fab10818e1344e3489bdd899aa9a81d24c7c
BL00DY-C0D3 <151839541+BL00DY-C0D3@users.noreply.github.com>
2024-01-12 14:10:55 +0100

    Update dummy.sh

--- scripts/dummy.sh
+++ scripts/dummy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-#create a virtual Screen
-Xvfb :0 -screen 0 1024x768x16 &
 #Only used as the entrypoint
 bash /scripts/error-check.sh
 #this is for running this script infinitely, so the container doesnt exit
