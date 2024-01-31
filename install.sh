0f1c63bf96ff45808ef70045d15be92edb485c3d
BL00DY-C0D3 <151839541+BL00DY-C0D3@users.noreply.github.com>
2024-01-31 14:14:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,98 @@ configJson='{
   "transactionservice_host": "_transactionservice_host"
 }'
 
+#This function asks the user if he wants to download Echo and if so, it downloads Echo
+function downloadEcho {
+    server1="rsync -rtz --progress --partial --compress-level=0 --password-file=./PASSWORD evr@echo.marceldomain.de::files"
+    server2="rsync -rtz --progress --partial --compress-level=0 --password-file=./PASSWORD evr@nakama0.eu-west.echovrce.com::files"
+    #check if he wants to download
+    echo "Do you want to Download the newest Echo Binarys? If you dont you need to provide them by your own."
+    read checkdownloadEcho
+    #checks if the answer is correct
+    if ! [[ "$checkdownloadEcho" =~ [yYnN]{1} ]]
+    then
+        echo "Wrong Input. Please try again."
+        checkdownloadEcho
+        return 160 # I started to choose random numbers due to laziness...
+    
+    fi
+    if [[ "$checkdownloadEcho" =~ [yY] ]]
+    then
+        echo "Echo will now be downloaded. We will test the Download-Speeds now to automatically choose the fastest server."
+        mkdir ./ready-at-dawn-echo-arena
+        #
+        installNeededSoftware
+        #CHECK DOWNLOAD SPEED
+        echo -e '\033[0;31m' #write in red
+        echo "Please do not abort! Depending on the connection to the server it could take like 2 min. The filesize is like 10mb btw."
+        echo -e '\033[0m' # No Color
+        rm ./temp/malgun.ttf 2> /dev/null
+        
+        #Check server 1
+        local downloadStart1=`date +%s.%N`
+        output1=$( $server1/content/engine/core/fonts/malgun.ttf ./temp/ | tee /dev/tty )
+        rm -r ./temp
+        local downloadEnd1=`date +%s.%N`
+        local speedtest1=$( echo "$downloadEnd1-$downloadStart1" | bc )
+        
+        echo -e '\033[0;31m' #write in red
+        echo "Next server will be tested now"
+        echo -e '\033[0m' # No Color
+        
+        #Check server 2
+        local downloadStart2=`date +%s.%N`S
+        output2=$( $server2/content/engine/core/fonts/malgun.ttf ./temp/ | tee /dev/tty )
+        rm -r ./temp
+        local downloadEnd2=`date +%s.%N`        
+        local speedtest2=$( echo "$downloadEnd1-$downloadStart1" | bc )
+        
+        choosenServer=0 #If this changes in the following "if", there is no need for a comparison
+        #Check if the Downloads succeeded
+        # if error in one of the rsyncs
+        if ! [[ "$output1" =~ 100\%.* ]] || ! [[ "$output2" =~ 100\%.* ]] 
+        then
+            if ! [[ "$output1" =~ 100\%.* ]]
+            then
+                choosenServer=2
+                if ! [[ "$output2" =~ 100\%.* ]]
+                then
+                    echo "No connection to any Download-Server possible. Please try again."
+                    echo -e '\033[0m' # No Color
+                    exit
+                fi
+            else
+                choosenServer=1
+            fi
+            echo $choosenServer
+        fi
+        
+
+        if [[ $choosenServer == 0 ]]
+        then
+            speedTestResult=$( echo "speedtest1 - speedtest2" | bc )
+            if [[ "$speedTestResult" =~ "-.*" ]]
+            then
+                choosenServer=2
+            else
+                choosenServer=1
+            fi
+        fi
+        
+        echo -e '\033[0;31m' #write in red
+        echo "The download of Echo will beginn now."
+        echo -e '\033[0m' # No Color
+        
+        if [[ $choosenServer == 1 ]]
+        then
+            $server1/. ./ready-at-dawn-echo-arena
+        else
+            $server2/. ./ready-at-dawn-echo-arena
+        fi
+    fi
+    secondInstallValue=true
+}
+
+
 
 #This function checks if the echo folder is available
 function checkForEchoFolder {
@@ -215,68 +307,87 @@ function checkOS {
 function installNeededSoftware {
     if [ "$osRelease" = "deb" ]
     then
-        # Add Docker's official GPG key:
         apt update
-        apt install -y ca-certificates curl gnupg
-        install -m 0755 -d /etc/apt/keyrings
-        curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-        chmod a+r /etc/apt/keyrings/docker.gpg
-        
-        # Add the repository to Apt sources:
-        echo \
-          "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-          $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-          tee /etc/apt/sources.list.d/docker.list > /dev/null
-        apt update
-        apt install -y lsof docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+        if ! [[ $secondInstallValue = true ]]
+        then
+            apt install -y rsync bc
+        else
+            # Add Docker's official GPG key:
+            apt install -y ca-certificates curl gnupg
+            install -m 0755 -d /etc/apt/keyrings
+            curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+            chmod a+r /etc/apt/keyrings/docker.gpg
+            
+            # Add the repository to Apt sources:
+            echo \
+              "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+              $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+              tee /etc/apt/sources.list.d/docker.list > /dev/null
+            apt update
+            apt install -y lsof docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+        fi
     elif [ "$osRelease" = "fed" ]
     then
-        #remove old Docker Versions
-        dnf -y remove docker \
-                  docker-client \
-                  docker-client-latest \
-                  docker-common \
-                  docker-latest \
-                  docker-latest-logrotate \
-                  docker-logrotate \
-                  docker-selinux \
-                  docker-engine-selinux \
-                  docker-engine
-        #add repo
-        dnf -y install dnf-plugins-core
-        dnf -y config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-        #install docker and lsof
-        dnf -y install lsof docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-        #start and enable docker
-        systemctl start docker
-        systemctl enable docker
+        if ! [[ $secondInstallValue = true ]]
+        then
+            yum install rsync bc
+        else
+            #remove old Docker Versions
+            dnf -y remove docker \
+                      docker-client \
+                      docker-client-latest \
+                      docker-common \
+                      docker-latest \
+                      docker-latest-logrotate \
+                      docker-logrotate \
+                      docker-selinux \
+                      docker-engine-selinux \
+                      docker-engine
+            #add repo
+            dnf -y install dnf-plugins-core
+            dnf -y config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+            #install docker and lsof
+            dnf -y install lsof docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+            #start and enable docker
+            systemctl start docker
+            systemctl enable docker
+        fi
     elif [ "$osRelease" = "cen" ]
     then
-        #remove old Docker Versions
-        yum -y remove docker \
-                  docker-client \
-                  docker-client-latest \
-                  docker-common \
-                  docker-latest \
-                  docker-latest-logrotate \
-                  docker-logrotate \
-                  docker-engine
-        #add repo
-        yum -y install -y yum-utils
-        yum-config-manager -y --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-        #install docker and lsof
-        yum -y install lsof docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-        #start and enable docker
-        systemctl start docker
-        systemctl enable docker
-        
+        if ! [[ $secondInstallValue = true ]]
+        then
+            yum install rsync bc
+        else
+            #remove old Docker Versions
+            yum -y remove docker \
+                      docker-client \
+                      docker-client-latest \
+                      docker-common \
+                      docker-latest \
+                      docker-latest-logrotate \
+                      docker-logrotate \
+                      docker-engine
+            #add repo
+            yum -y install -y yum-utils
+            yum-config-manager -y --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+            #install docker and lsof
+            yum -y install lsof docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+            #start and enable docker
+            systemctl start docker
+            systemctl enable docker
+        fi
     elif [ "$osRelease" = "arc" ]
     then
-        #Install Docker and lsof
-        pacman --noconfirm -S docker lsof
-        #Start and enable docker
-        systemctl start docker
-        systemctl enable docker
+        if ! [[ $secondInstallValue = true ]]
+        then
+             pacman --noconfirm -S rsync bc
+        else
+            #Install Docker and lsof
+            pacman --noconfirm -S docker lsof
+            #Start and enable docker
+            systemctl start docker
+            systemctl enable docker
+        fi
     fi
 }
 
@@ -351,10 +462,9 @@ trap ctrl_c SIGINT
 
 
 
-
-
-
 #Start all functions
+checkOS
+downloadEcho
 checkForEchoFolder
 checkIfUserWantsConfigure
 if [[ "$askconfigure" =~ [yY]{1} ]]
@@ -364,7 +474,6 @@ then
 fi
 getRegion
 writeRegion
-checkOS 
 installNeededSoftware
 buildPackage
 startContainer
