41184659f6bc0d0fd091784641cbdd37816f02d7
BL00DY-C0D3 <151839541+BL00DY-C0D3@users.noreply.github.com>
2024-05-08 20:20:17 +0200

    Update dummy.sh

--- scripts/dummy.sh
+++ scripts/dummy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Only used as the entrypoint
-bash /scripts/error-check.sh
+bash /scripts/error-check.sh &
+bash /scripts/checkForStuckServer.sh &
 #this is for running this script infinitely, so the container doesnt exit
 tail -f /dev/null
