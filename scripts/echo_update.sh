4193a7822b751ff43a969067cdc2febe30b816b3
BL00DY-C0D3 <151839541+BL00DY-C0D3@users.noreply.github.com>
2024-05-08 21:31:34 +0200

    Update echo_update.sh

--- scripts/echo_update.sh
+++ scripts/echo_update.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 cd $(readlink -f $(dirname $0))
-shutdownMessages=('[ECHORELAY.GAMESERVER] Signaling end of session'
-'[NSLOBBY] registration successful'
-'[NETGAME] NetGame switching state (from logged in, to lobby)'
-'[TCP CLIENT] [R14NETCLIENT] connection to'
-'[LEVELLOAD] Unloading level')
+
 rsyncCommand="rsync -crtz --exclude-from="../files/exclude.list" --progress --partial --compress-level=0 "
 rsyncUpdateCheckCommand="rsync -cnrv --exclude-from="../files/exclude.list" "
 #Path of the echo folder
 echoPath="../ready-at-dawn-echo-arena"
-#The name of the Containers
-containerName=$( grep dockerContainerName= ../run.sh | sed -e "s/dockerContainerName=\"//g" -e "s/\"//g" )
 
-function startServer {
-    a=0
-    while [[ $a -lt $amountRunningServer ]]
-    do
-        bash ../run.sh
-        ((a++))
-        sleep 5
-    done    
-}
+
 
 function updateEcho {
     if ! [[ -f ../rsyncHosts ]]
@@ -79,49 +65,8 @@ function updateEcho {
     #Start the download
     $rsyncCommand${rsyncHosts[$fastestServer]}/. ../ready-at-dawn-echo-arena       
     #start the servers back up
-    startServer
+    source ./slowlyCloseServers.sh --start
 }
-
-#This function slowly closes the server if no one is on it.
-function slowlyCloseServers {
-        #Save each Docker Container ID in an array
-        mapfile -t runningContainer < <(docker ps --filter "ancestor=$containerName" --format "{{.ID}}")
-        
-        if ! [[ $secondRun ]]
-        then
-            #save the amount of running servers        
-            declare -g amountRunningServer=${#runningContainer[@]}
-            echo $amountRunningServer "Servers are running. We will slowly close them now."
-            secondRun=1
-        fi
-        
-        #check logs of each Container to close it.
-        for id in ${runningContainer[*]}
-        do
-            toClose=0
-            #get last 10 lines and check each one
-            logContent=$(tail -5 $echoPath/logs/$id/*.log) 
-            for message in "${shutdownMessages[@]}"
-            do
-                if echo "$logContent" | grep -q -F "$message"
-                then
-                    toClose=1
-                fi
-            done
-            #if [[ $logContent =~ ${shutdownMessages[@]} ]]
-            #then
-            #    toClose=1
-            #fi
-            
-            if [[ $toClose == 1 ]]
-            then
-                docker stop $id
-            fi
-            
-        done
-        
-}
-
 
 
 #This function checks if an update is available
@@ -167,11 +112,7 @@ function checkForUpdates {
     done
 
     echo "Updates found"
-    while [[ $( docker ps --filter "ancestor=$containerName" --format "{{.ID}}" | wc -l ) -gt 0 ]]
-    do
-        slowlyCloseServers
-        sleep 1
-    done
+        source ./slowlyCloseServers.sh
     #start the Update
     updateEcho
 }
