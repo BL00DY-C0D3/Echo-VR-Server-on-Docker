e73d05cff6ed3fb6e53ad434fc0b7f3053e39ac5
BL00DY-C0D3 <151839541+BL00DY-C0D3@users.noreply.github.com>
2024-07-27 20:14:54 +0200

    Create start-echo_arm.sh

---
+++ scripts/start-echo_arm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#here you need to set the parameters you want to run echo with
+#Regions
+#"uscn", // US Central North (Chicago)
+#"us-central-2", // US Central South (Texas)
+#"us-central-3", // US Central South (Texas)
+#"use", // US East (Virgina)
+#"usw", // US West (California)
+#"euw", // EU West 
+#"jp", // Japan (idk)
+#"sin", // Singapore oce region
+region='euw'
+#$port is set as an environment variable
+flags="-noovr -server -headless -timestep 120 -fixedtimestep -nosymbollookup -port $port -logpath logs/$HOSTNAME -noconsole -serverregion $region"
+
+#create the Log directory 
+mkdir -p /ready-at-dawn-echo-arena/logs/$HOSTNAME/old 2> /dev/null
+#move old log files
+mv /ready-at-dawn-echo-arena/logs/$HOSTNAME/*.log /ready-at-dawn-echo-arena/logs/$HOSTNAME/old
+
+#start the echo server process
+nohup /usr/local/bin/wine64 /ready-at-dawn-echo-arena/bin/win10/echovr.exe $flags &
